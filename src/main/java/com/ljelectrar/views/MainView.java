82d0b6bc23848cfb4238f17c02eb146454cb2bd7
Leandro Junior Alves Dos Santos <ljelectrar@outlook.com>
2023-12-29 14:50:49 -0300

    Start simple project

---
+++ src/main/java/com/ljelectrar/views/MainView.java
@@ -0,0 +1,13 @@
+package com.ljelectrar.views;
+
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+
+@Route("/index")
+@PageTitle(value="Home")
+public class MainView extends VerticalLayout {
+	
+	
+	
+}
