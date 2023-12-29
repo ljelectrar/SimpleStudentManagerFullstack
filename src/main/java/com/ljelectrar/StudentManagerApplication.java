5f7ee52f5c182997f04521348c68f492418ad07c
Leandro Junior Alves Dos Santos <ljelectrar@outlook.com>
2023-12-29 15:06:49 -0300

    Hello World with MainView using Vaadin, initial test and config

--- src/main/java/com/ljelectrar/StudentManagerApplication.java
+++ src/main/java/com/ljelectrar/StudentManagerApplication.java
@@ -3,8 +3,13 @@ package com.ljelectrar;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.theme.Theme;
+import com.vaadin.flow.theme.lumo.Lumo;
+
 @SpringBootApplication
-public class StudentManagerApplication {
+@Theme(themeClass = Lumo.class)
+public class StudentManagerApplication implements AppShellConfigurator {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentManagerApplication.class, args);
--- src/main/java/com/ljelectrar/views/MainView.java
+++ src/main/java/com/ljelectrar/views/MainView.java
@@ -1,13 +1,19 @@
 package com.ljelectrar.views;
 
+import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
-@Route("/index")
+@Route(value="/index")
 @PageTitle(value="Home")
 public class MainView extends VerticalLayout {
 	
-	
+	public MainView() {
+		add(new Button("Click", event ->{
+			add(new Text("Hello World"));
+		}));
+	}
 	
 }
