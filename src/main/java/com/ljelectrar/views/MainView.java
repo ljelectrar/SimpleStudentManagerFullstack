d01ac50bbdf75ae81d0310bf2901ac8a7f0d787a
Leandro Junior Alves Dos Santos <ljelectrar@outlook.com>
2023-12-30 11:31:56 -0300

    Added icons to stataus column

--- src/main/java/com/ljelectrar/views/MainView.java
+++ src/main/java/com/ljelectrar/views/MainView.java
@@ -6,6 +6,8 @@ import java.util.List;
 import com.ljelectrar.models.Status;
 import com.ljelectrar.models.Student;
 import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
@@ -26,10 +28,38 @@ public class MainView extends VerticalLayout {
 		
 		createFieldVariables();
 		
-		add(logoLayout ,grid);
-		
 		loadStudents();
+		configureGrid();
 		
+		add(logoLayout ,grid);
+	}
+	
+	private void configureGrid() {
+		grid.setSizeFull();
+		grid.setColumns("country", "zipcode");
+		grid.addColumn(s-> s.getName()).setHeader("name");
+		grid.addColumn(s -> s.getAge()).setHeader("Age");
+		
+		grid.addComponentColumn(s -> {
+			//using different colored icons
+			// to different status
+			Icon icon;
+			
+			if(s.getStatus().equals("ACTIVE")) {
+				icon = VaadinIcon.CIRCLE.create();
+				icon.setColor("green");
+			} else if (s.getStatus().equals("PASSIVE")) {
+				icon = VaadinIcon.CLOSE_CIRCLE.create();
+				icon.setColor("red");
+			} else {
+				icon = VaadinIcon.CHECK_CIRCLE.create();
+				icon.setColor("orange");
+			}
+			
+			return icon;
+		}).setHeader("Status");
+		
+		grid.getColumns().forEach(col -> col.setAutoWidth(true));
 	}
 	
 	private void createFieldVariables() {
@@ -39,7 +69,9 @@ public class MainView extends VerticalLayout {
 	
 	private void loadStudents() {
 		List<Student> students = new ArrayList<>();
-		students.add(new Student("Leandro Junior", 28, 27257515, "BR", new Status("Reactive")));
+		students.add(new Student("Leandro Junior", 28, 27257515, "BR", new Status("ACTIVE")));
+		students.add(new Student("Maiara", 21, 27257500, "BR", new Status("PASSIVE")));
+		students.add(new Student("Julia", 26, 27257500, "BR", new Status("ABSOLVED")));
 		grid.setItems(students);
 	}	
 	
